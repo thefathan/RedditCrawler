1b2e55b436abe8517229770cbeafa854ba75fef2
thefathan <fathanananta@gmail.com>
2023-02-03 17:32:22 +0700

    pindah folder sebelah

--- mywork/redditscraper.py
+++ mywork/redditscraper.py
@@ -7,9 +7,10 @@ reddit = praw.Reddit(client_id = 'DnpX9tZO75idVbdUuDUgdg',
                     password = 'FathanReddit123-',
                     user_agent= 'tugasakhirv1')
 
-subreddit = reddit.subreddit('python')
+subreddit = reddit.subreddit('cybersecurity')
 
 hot_scrape = subreddit.hot(limit=5)
 
 for submission in hot_scrape:
-    print(submission)
+    if not submission.stickied:
+        print(submission.title)
